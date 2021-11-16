25fd10c13c2eb6ddac5412fd6bad932974d3ec0e
ada99tf <69634645+ada99tf@users.noreply.github.com>
2021-11-16 20:31:47 +0100

    Update main.py
    El comando !ping, deberia mostar pong y los ms que ha tardado :)

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,6 +1,7 @@
 #Librerias.
 import discord
 from discord.ext import commands
+import time
 # Prefix
 bot =  commands.Bot(command_prefix='!', description='Bot de clase...')
 
@@ -8,13 +9,21 @@ bot =  commands.Bot(command_prefix='!', description='Bot de clase...')
 @bot.command()
 
 async def ping(ctx):
-    await ctx.send('pong')
+    ms1= time.monotonic()
+    mensaje = await ctx.send('Pong! ')
+    ms2 = (time.monotonic() - ms1)*1000
+    ms = (str(ms2).split("."))[0]
+    await mensaje.edit(content="Pong! " + ms + " ms.")
+
+
 
 #Eventos.
 @bot.event
 async def on_ready():
     await bot.change_presence(status="Ready!")
     print('El bot esta Listo.')
+
+   
 
 
 #Token bot.
